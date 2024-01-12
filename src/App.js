845db0b27bc7dd756bdcf32df7719656d91b26bf
Jameson Hill <jhill@achieveit.com>
2024-01-12 13:31:48 -0500

    Add autoHeight

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import './App.css';
+import { useState } from 'react';
 import '@bryntum/gantt/gantt.stockholm.css';
 import { BryntumGantt } from '@bryntum/gantt-react';
 
 function App() {
-  const config = {
+  const [config] = useState(() => ({
     columns: [
       {
         type: 'name',
         field: 'name',
+        autoHeight: true,
+        width: 100,
         renderer: (args) => {
-          console.log('yo');
           return <div>{args.value}</div>;
         },
         sortable: false
       }
     ]
-  };
+  }))
 
   return (
       <div>
-        <BryntumGantt {...config} project={{ tasks: [{ id: '1', name: 'Hello', startDate: new Date(), endDate: new Date()}] }} projectLinesFeature={false} />
+        <BryntumGantt {...config} project={{ tasks: [{ id: '1', name: 'Hello here is a really long name that I want to wrap text with less width', startDate: new Date(), endDate: new Date()}] }} projectLinesFeature={false} />
       </div>
   );
 }
