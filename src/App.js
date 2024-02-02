de88be8b7abbafce4c7386738018d79d129db8dd
Jameson Hill <jhill@achieveit.com>
2024-02-02 15:13:09 -0500

    Add viewPreset and timespan

--- src/App.js
+++ src/App.js
@@ -45,25 +45,47 @@ function BryntumComponent({ data }) {
         }} 
           columns={columns}
           project={{ tasks }} 
-          projectLinesFeature={false} 
+          projectLinesFeature={false}
+          viewPreset={{
+            timeResolution: { unit: 'day', increment: 1 },
+            headers: [
+              { unit: 'year', dateFormat: 'YYYY' },
+              {
+                unit: 'quarter',
+                dateFormat: 'Q',
+                renderer: (date, __, { value }) => {
+                  return `Q${value} ${date.getFullYear()}`;
+                }
+              },
+              {
+                unit: 'month',
+                dateFormat: 'MMM'
+              }
+            ]
+          }}
         />
+        <button onClick={() => {
+          if (ganttRef.current) {
+            ganttRef.current.instance.setTimeSpan(new Date(2021, 5, 15), new Date(2021,9, 30));
+          }
+        }}>Set Timespan to minDate</button>
       </div>
   );
 }
 
 function App() {
-  const [data, setData] = useState([{ id: 1, name: 'task 1', custom: 'my custom thing'}]);
+  const [data, setData] = useState([{ id: 1, name: 'task 1', custom: 'my custom thing', startDate: new Date(2021, 5, 15), endDate: new Date(2021,8, 30)}]);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      console.log('in timer func');
-      setData((prev) => [{ id: 1, name: prev[0].name + '1', custom: prev[0].custom + '1' }]);
-    }, 2000);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     console.log('in timer func');
+  //     setData((prev) => [{  id: 1, name: prev[0].name + '1', custom: prev[0].custom + '1' }]);
+  //   }, 2000);
 
-    return () => {
-      clearInterval(interval);
-    }
-  }, []);
+  //   return () => {
+  //     clearInterval(interval);
+  //   }
+  // }, []);
 
   return (
     <div>
